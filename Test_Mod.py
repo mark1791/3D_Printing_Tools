5b775a737b860f7a1d98b842f3b618d0da4513bc
mark1791 <47164264+mark1791@users.noreply.github.com>
2019-05-21 13:24:34 +0100

    Revert "Update added Bounding Box"
    This reverts commit 390dc422f399d474058309bdcf9850f2cad041ab.

--- Test_Mod.py
+++ Test_Mod.py
@@ -4,15 +4,15 @@ import _SMutils
 import random
 import Part
 
-TOOL_ICON = _SMutils.iconsPath() + '/Test.svg'
+TOOL_ICON = _SMutils.iconsPath() + '\Test.svg'
 
 class TestCmd: 
-	def __init__(self, changevalue):
-        	#changeval = ""
-        	self.changevalue = changevalue
-        	#FreeCAD.Console.PrintMessage(self.changevalue)
+   def __init__(self, changevalue):
+        #changeval = ""
+        self.changevalue = changevalue
+        #FreeCAD.Console.PrintMessage(self.changevalue)
 		
-	def Activated(self): 
+   def Activated(self): 
 		# Script to Scale Imperial mesh to metric
 		FreeCAD.Console.PrintMessage('Random Colour Change Started\n')
 		#changeval = str(self.changevalue)
@@ -25,43 +25,43 @@ class TestCmd:
 			changeval = str(self.changevalue)
 			#FreeCAD.Console.PrintMessage(changeval + " XXXX\n")
 			if self.changevalue == "Rand_Col":
-				#set doc to active document
-				doc=App.activeDocument()
+			   #set doc to active document
+			   doc=App.activeDocument()
 			   
-				new_name = obj.Name+"_Solid_"+str(random.randrange(10**4, 10**5))
-				box = doc.addObject("Part::Box",new_name) # Step 1
-				#FreeCAD.Console.PrintMessage(obj.Name + "\n")
-				#Step 1 Create Part Shape
-				box = Part.Shape()
-				#Step 2 Convert Mesh to temporary part shape
-				box.makeShapeFromMesh(FreeCAD.ActiveDocument.getObject(obj.Name).Mesh.Topology,0.100000) # Step 3
-				#Step 3 move temporary shape to new object
-				#FreeCAD.getDocument("Unnamed").getObject(new_name).Shape=box
-				FreeCAD.ActiveDocument.getObject(new_name).Shape=box
-				#Step 4 Purge the new solid
-				#FreeCAD.getDocument("Unnamed").getObject(new_name).purgeTouched()
-				FreeCAD.ActiveDocument.getObject(new_name).purgeTouched()
-				#Step 5 Delete the temporary object
-				del box
-				#Step 6 refine mesh -  App.ActiveDocument.addObject('Part::Feature','Mesh_Solid').Shape=App.ActiveDocument.Mesh_Solid.Shape.removeSplitter() refine mesh4
-				#App.ActiveDocument.addObject('Part::Feature','Mesh_Solid').Shape=App.ActiveDocument.getObject(new_name).Shape.removeSplitter() #refine mesh
-				doc.addObject('Part::Feature',"Refined_"+new_name).Shape=doc.getObject(new_name).Shape.removeSplitter()
-				#FreeCAD.ActiveDocument.getObject(new_name).hide()
-				FreeCAD.ActiveDocument.recompute()
-				FreeCAD.Console.PrintMessage(self.changevalue + " Does this show\n")
-				FreeCAD.Console.PrintMessage(obj.Name + "\n")
+			   new_name = obj.Name+"_Solid_"+str(random.randrange(10**4, 10**5))
+			   box = doc.addObject("Part::Box",new_name) # Step 1
+			   #FreeCAD.Console.PrintMessage(obj.Name + "\n")
+			   #Step 1 Create Part Shape
+			   box = Part.Shape()
+			   #Step 2 Convert Mesh to temporary part shape
+			   box.makeShapeFromMesh(FreeCAD.ActiveDocument.getObject(obj.Name).Mesh.Topology,0.100000) # Step 3
+			   #Step 3 move temporary shape to new object
+			   #FreeCAD.getDocument("Unnamed").getObject(new_name).Shape=box
+			   FreeCAD.ActiveDocument.getObject(new_name).Shape=box
+			   #Step 4 Purge the new solid
+			   #FreeCAD.getDocument("Unnamed").getObject(new_name).purgeTouched()
+			   FreeCAD.ActiveDocument.getObject(new_name).purgeTouched()
+			   #Step 5 Delete the temporary object
+			   del box
+			   #Step 6 refine mesh -  App.ActiveDocument.addObject('Part::Feature','Mesh_Solid').Shape=App.ActiveDocument.Mesh_Solid.Shape.removeSplitter() refine mesh4
+			   #App.ActiveDocument.addObject('Part::Feature','Mesh_Solid').Shape=App.ActiveDocument.getObject(new_name).Shape.removeSplitter() #refine mesh
+			   doc.addObject('Part::Feature',"Refined_"+new_name).Shape=doc.getObject(new_name).Shape.removeSplitter()
+			   #FreeCAD.ActiveDocument.getObject(new_name).hide()
+			   FreeCAD.ActiveDocument.recompute()
+			   FreeCAD.Console.PrintMessage(self.changevalue + " Does this show\n")
+			   FreeCAD.Console.PrintMessage(obj.Name + "\n")
 			
-				#Change Object Colour Randomly
+			   #Change Object Colour Randomly
 			
-				 #FreeCADGui.ActiveDocument.getObject(obj.Name).ShapeColor = (Colour_ran1,Colour_ran2,Colour_ran3)
+			   #FreeCADGui.ActiveDocument.getObject(obj.Name).ShapeColor = (Colour_ran1,Colour_ran2,Colour_ran3)
 			
-			FreeCAD.Console.PrintMessage('Object Processed ' + str(count) + '\n')
+			   FreeCAD.Console.PrintMessage('Object Processed ' + str(count) + '\n')
 			count = count + 1
 				
 		FreeCAD.Console.PrintMessage('Colours Changed Successfully')
 
-	def GetResources(self): 
-		return {'Pixmap' : TOOL_ICON, 'MenuText': 'Short text', 'ToolTip': 'Test Icon Module to test new code before implementation into master code base'} 
+   def GetResources(self): 
+       return {'Pixmap' : TOOL_ICON, 'MenuText': 'Short text', 'ToolTip': 'Test Icon Module to test new code before implementation into master code base'} 
        
 
        
